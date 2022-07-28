65594a2a656a16dd0369ae489e400d7b8ca7ab73
mozhenghua <baisui@qlangtech.com>
2022-07-28 14:02:14 +0800

    fix scala2.11 release confict

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-mvn compile install -Dfast -Dscala-2.12 -DskipTests=true -DaltDeploymentRepository=base::default::http://localhost:8080/release
+#mvn compile install -Dfast -Dscala-2.12 -DskipTests=true -DaltDeploymentRepository=base::default::http://localhost:8080/release
+
+mvn compile deploy -Pscala-2.11 -Dfast -DskipTests=true -DaltDeploymentRepository=base::default::http://localhost:8080/release
