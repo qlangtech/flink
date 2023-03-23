04f654456c4ce3e53e989dc4fb7103930702240d
mozhenghua <baisui@qlangtech.com>
2023-03-23 16:28:48 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,22 @@
-#mvn compile install -Dfast -Dscala-2.12 -DskipTests=true -DaltDeploymentRepository=base::default::http://localhost:8080/release
+#!/usr/bin/env bash
 
-mvn compile deploy -Pscala-2.11 -Dfast -DskipTests=true -DaltDeploymentRepository=base::default::http://localhost:8080/release
+#mvn compile install -Dfast -Dscala-2.12 -DskipTests=true -DaltDeploymentRepository=base::default::http://localhost:8080/release
+scala_version=2.11
+
+mvn compile deploy -Pscala-${scala_version} -Dfast -DskipTests=true -DaltDeploymentRepository=base::default::http://localhost:8080/release \
+-pl flink-kubernetes\
+,flink-table/flink-table-planner-blink\
+,flink-table/flink-table-runtime-blink\
+,flink-test-utils-parent/flink-test-utils\
+,flink-test-utils-parent/flink-connector-test-utils\
+,flink-core\
+,flink-streaming-java\
+,flink-table/flink-table-common\
+,flink-tests\
+,flink-runtime\
+,flink-streaming-scala\
+,flink-clients\
+,flink-connectors/flink-connector-jdbc\
+,flink-table/flink-table-api-java-bridge\
+,flink-connectors/flink-connector-kafka\
+ -am
